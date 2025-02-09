f266681c3e51bebd40fb7d7f6a1a0437d97b8e90
tgraber4 <tgraber4@gmail.com>
2025-02-08 22:00:34 -0600

    Created Book class and constructors

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+
+public class Book {
+	private String title;
+	
+	private String author;
+	
+	private String ISBN;
+	
+	private double price;
+
+	public Book(String title, String author, String ISBN, double price) {
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.price = price;
+	}
+
+	public Book() {
+		
+	}
+	
+	
+}
