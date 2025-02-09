ee7120ceec33598c24c4b7f2ca332de23ef53156
tgraber4 <tgraber4@gmail.com>
2025-02-08 22:16:01 -0600

    Added getters and setters for Book class, added constructors and documentation to Book class, and created the LibraryApp class

--- src/Book.java
+++ src/Book.java
@@ -8,6 +8,14 @@ public class Book {
 	
 	private double price;
 
+	/**
+	 * Creates a Book object using the items: title, author, ISBN, and price.
+	 * 
+	 * @param title
+	 * @param author
+	 * @param ISBN
+	 * @param price
+	 */
 	public Book(String title, String author, String ISBN, double price) {
 		this.title = title;
 		this.author = author;
@@ -15,9 +23,63 @@ public class Book {
 		this.price = price;
 	}
 
+	/**
+	 * Creates a Book objects without any parameters.
+	 * 
+	 * Sets title, author, and ISBN to "Unknown"<br>
+	 * Sets price to 0.0
+	 */
 	public Book() {
-		
+		this.title = "Unknown";
+		this.author = "Unknown";
+		this.ISBN = "Unknown";
+		this.price = 0.0;
 	}
+	/**
+	 * Creates a copy of a given Book object.
+	 * 
+	 * @param u - a Book object
+	 */
+	public Book(Book u) {
+		this.title = u.getTitle();
+		this.author = u.getAuthor();
+		this.ISBN = u.getISBN();
+		this.price = u.getPrice();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 	
 	
 }
