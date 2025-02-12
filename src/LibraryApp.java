b4da0bef2d827d9f014b4412e6591a39229bbc75
coltonmizeur <coltonmmizeur@gmail.com>
2025-02-12 16:23:25 -0600

    Added library and three books

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -2,8 +2,13 @@
 public class LibraryApp {
 
 	public static void main(String[] args) {
-		System.out.println("test");
-		System.out.println("test2");
+		
+		Library Ellis_Library = new Library();
+		Book Book_One = new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780333791035", 5.00);
+		Book Book_Two = new Book("One Flew Over the Cuckoo's Nest", "Ken Kesey", "9780330235648", 9.18);
+		Book Book_Three = new Book("1984", "George Orwell", "9780155658110", 2.24);
+		
+		
 	}
 
 }
