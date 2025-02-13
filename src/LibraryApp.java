0a5125c8c0a5e25b7b6037a2d6d0f312ab3b9953
coltonmizeur <coltonmmizeur@gmail.com>
2025-02-12 19:40:03 -0600

    adds books into library

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,13 +1,15 @@
-
 public class LibraryApp {
 
 	public static void main(String[] args) {
 		
 		Library Ellis_Library = new Library();
-		Book Book_One = new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780333791035", 5.00);
-		Book Book_Two = new Book("One Flew Over the Cuckoo's Nest", "Ken Kesey", "9780330235648", 9.18);
-		Book Book_Three = new Book("1984", "George Orwell", "9780155658110", 2.24);
+		Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780333791035", 5.00);
+		Book book2 = new Book("One Flew Over the Cuckoo's Nest", "Ken Kesey", "9780330235648", 9.18);
+		Book book3 = new Book("1984", "George Orwell", "9780155658110", 2.24);
 		
+		Ellis_Library.addBook(book1);
+		Ellis_Library.addBook(book2);
+		Ellis_Library.addBook(book3);
 		
 	}
 
